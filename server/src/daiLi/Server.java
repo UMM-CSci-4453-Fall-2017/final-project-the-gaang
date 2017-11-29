9df7a12056f616a8bd5728fe13280e7bfd848a15
nathan-beneke <benek020@morris.umn.edu>
2017-11-29 16:37:53 -0600

    create project structure

---
+++ server/src/daiLi/Server.java
@@ -0,0 +1,9 @@
+package daiLi;
+//TODO import Spark stuff
+//TODO import MongoDB stuff
+
+public class Server{
+    public static void Main(String[] args){
+        //TODO Start listening on some port, write methods/routes
+    }
+}
