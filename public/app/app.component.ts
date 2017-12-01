110e2e8cb26de998d77410b7a070b42640ef7ca4
nathan-beneke <benek020@morris.umn.edu>
2017-12-01 17:21:19 -0600

    Update file structure

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: 'app.component.html'
+    selector: 'app',
+    templateUrl: 'app.component.html'
 })
 
-export class AppComponent{}
+// App Component class
+export class AppComponent {
+
+    constructor() { }
+}
---
+++ public/app/app.routes.ts
@@ -0,0 +1,9 @@
+// Imports
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Route Configuration
+export const routes: Routes = [
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
