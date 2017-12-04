9e197fc1021ec9af6001da14e2468555c27b452e
cookx876 <cookx876@morris.umn.edu>
2017-12-04 17:34:25 -0600

    Add routes for finding people and organizations. (currently untested, for the most part)

--- express.js
+++ express.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongo = require('mongodb');
 Promise = require('bluebird');
 var using = Promise.using;
+const bodyParser = require('body-parser');
 
 app = express();
 port = process.env.PORT || 1337;
@@ -9,5 +10,26 @@ port = process.env.PORT || 1337;
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
+async function run(){
+    const db = mongodb.mongoClient.connect('mongodb:<ip:port/databaseName>'); // replace with real stuff, don't commit ip. Look at lab 10 for ip and port
+    const app = express();
+    app.use(bodyParser.json());
 
-//TODO write all of the routes
+
+var getById = function(collection, orgID){
+    return JSON.stringify(db.getCollection(collection).find(_id = orgID));
+}
+
+
+app.get("/organization/:orgID", function(req,res){
+    var orgID = req.param("orgID");
+    res.send(getById("AtLAOrganizations", orgID));
+}
+
+app.get("/people/:personID", function(req, res){
+    var id = req.param("personID");
+    res.send(getById("AtLAPeople", id));
+}
+
+//TODO write all of the routes/
+}
