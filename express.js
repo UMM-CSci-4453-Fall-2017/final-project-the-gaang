98877bb5a1a07a7e57572771622c34d9803fb5d0
nathan-beneke <benek020@morris.umn.edu>
2017-12-06 16:25:42 -0600

    fix issues with express

--- express.js
+++ express.js
@@ -1,35 +1,42 @@
 var express = require('express');
-var mongo = require('mongodb');
+var mongodb = require('mongodb');
 Promise = require('bluebird');
 var using = Promise.using;
 const bodyParser = require('body-parser');
 
-app = express();
-port = process.env.PORT || 1337;
+// run().catch(error => console.error(error));
 
-app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
-async function run(){
-    const db = mongodb.mongoClient.connect('mongodb:<ip:port/databaseName>'); // replace with real stuff, don't commit ip. Look at lab 10 for ip and port
+
+    // app = express();
+    port = process.env.PORT || 1337;
+    //
+    // app.use(express.static(__dirname + '/public'));
+    //
+    // app.listen(port);
     const app = express();
     app.use(bodyParser.json());
 
-
-var getById = function(collection, orgID){
-    return JSON.stringify(db.getCollection(collection).find(_id = orgID));
-}
+// var run = function() {
+    const db = mongodb.MongoClient.connect('mongodb://<ip:port/databaseName>'); // replace with real stuff, don't commit ip. Look at lab 10 for ip and port
 
 
-app.get("/organization/:orgID", function(req,res){
-    var orgID = req.param("orgID");
-    res.send(getById("AtLAOrganizations", orgID));
-}
+    var getById = function (collection, orgID) {
+        return JSON.stringify(db.getCollection(collection).find(_id = orgID));
+    };
 
-app.get("/people/:personID", function(req, res){
-    var id = req.param("personID");
-    res.send(getById("AtLAPeople", id));
-}
+// }
+
+    app.get("/organization/:orgID", function (req, res) {
+        var orgID = req.param("orgID");
+        res.send(getById("AtLAOrganizations", orgID));
+    });
+
+    app.get("/people/:personID", function (req, res) {
+        var id = req.param("personID");
+        res.send(getById("AtLAPeople", id));
+    });
 
 //TODO write all of the routes/
-}
+
+app.listen(port);
