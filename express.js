3ec11b4748892d76e1a3cfaca4c0ca838ca86956
cookx876 <cookx876@morris.umn.edu>
2017-12-10 17:30:37 -0600

    set up express and node, preliminary route for individual info pages

--- express.js
+++ express.js
@@ -1,35 +1,61 @@
 var express = require('express');
-var mongo = require('mongodb');
+mysql=require('mysql');
+var creds=require('./credentials.json');
+//var mongo = require('mongodb');
 Promise = require('bluebird');
 var using = Promise.using;
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 app = express();
 port = process.env.PORT || 1337;
 
-app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
-async function run(){
-    const db = mongodb.mongoClient.connect('mongodb:<ip:port/databaseName>'); // replace with real stuff, don't commit ip. Look at lab 10 for ip and port
-    const app = express();
-    app.use(bodyParser.json());
+creds.host='ids.morris.umn.edu';
+
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
+var connection = mysql.createConnection(creds);
+var db = "atla";
+var pool = mysql.createPool(creds);
+
+var getConnection = function(){
+    return pool.getConnectionAsync().disposer(
+        function(connection){return connection.release();}
+    );
+};
+
+var query = function(sql){
+    return using(getConnection(), function(connection){
+        return connection.queryAsync(sql);
+    });
+};
+
+var endPool = function(){
+    pool.end(function(err){});
+};
+
+var getDatabase = function(){
+    var toReturn = query("USE " + db + ";");
+    console.log(toReturn);
+    return toReturn;
+};
+
+app.use(express.static(__dirname+ '/public'));
+app.use(express.urlencoded()); // not entirely sure about this part here
+
+app.get("/entity/:id", function(req, res){
+    var id = req.params.id;
+    var sql = 'Select * from ' + db + '.entities;';
+    query(sql).then(function(results){ 
+        res.send(results); 
+        console.log(results); 
+        endPool;
+    });
+});
 
 
-var getById = function(collection, orgID){
-    return JSON.stringify(db.getCollection(collection).find(_id = orgID));
-}
-
-
-app.get("/organization/:orgID", function(req,res){
-    var orgID = req.param("orgID");
-    res.send(getById("AtLAOrganizations", orgID));
-}
-
-app.get("/people/:personID", function(req, res){
-    var id = req.param("personID");
-    res.send(getById("AtLAPeople", id));
-}
 
 //TODO write all of the routes/
-}
+
+app.listen(port);
